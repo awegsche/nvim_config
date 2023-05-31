306c7a3fc298f0acc34118cba524adee749a9f5b
Andreas Wegscheider <a.wegscheider7141@gmail.com>
2023-05-31 10:22:00 +0200

    add `treesitter-context`

--- lua/awegsche/packer.lua
+++ lua/awegsche/packer.lua
@@ -17,6 +17,7 @@ return require('packer').startup(function(use)
     }
 
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter-context')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
     use('theprimeagen/harpoon')
